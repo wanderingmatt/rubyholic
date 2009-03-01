3ecc783172c0cd65bc63eed2d398f99f5b59ac71
Matthew Anderson <manderson@onehub.com>
2009-03-01 15:41:29 -0800

    Fleshing out some event tests - can't get sorting tested properly.

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -6,4 +6,37 @@ class EventTest < ActiveSupport::TestCase
     assert ! event.valid?
     assert event.errors.on(:group_id)
   end
+  
+  test "event validates presence of location" do
+    event = Event.new
+    assert ! event.valid?
+    assert event.errors.on(:location_id)
+  end
+  
+  test "event validates presence of start_time" do
+    event = Event.new
+    assert ! event.valid?
+    assert event.errors.on(:start_time)
+  end
+  
+  test "event validates presence of end_time" do
+    event = Event.new
+    assert ! event.valid?
+    assert event.errors.on(:end_time)
+  end
+  
+  test "sort should return the correct order" do    
+    actual = Event.sort(1, 'groups.name')
+    
+    expected = [
+      events(:three).id,
+      events(:two).id,
+      events(:four).id,
+      events(:five).id,
+      events(:one).id,
+      events(:one).id
+    ]
+    
+    assert expected, actual
+  end
 end
