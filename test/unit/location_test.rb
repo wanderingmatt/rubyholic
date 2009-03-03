192040fa8be0d9c1a4da16889c2e64e660e72f53
Mike Tierney <mike@panpainter.com>
2009-03-03 14:00:28 -0800

    Location unit tests are now passing.

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class LocationTest < ActiveSupport::TestCase
   test "location validates presence of name" do
     location = Location.new
+    location.address = locations(:one).address
+    
     assert ! location.valid?
     assert location.errors.on(:name)
   end
@@ -14,7 +16,9 @@ class LocationTest < ActiveSupport::TestCase
   end
   
   test "should auto-geocode address" do
-    location = Location.new(locations(:one))
+    location = Location.create(:name => locations(:one).name, :address => locations(:one).address)
+    assert location.valid?
+    
     assert_equal 47.5798527, location.latitude
     assert_equal -122.1456091, location.longitude
   end
