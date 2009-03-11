646565e44bf6cbfa3ff8fd40de2a8f81ead053ca
Mike Tierney <mike@panpainter.com>
2009-03-11 06:55:52 -0700

    Updating the config file to include a requirement for Flexmock.  Doing some behind-the-scenes cleanup.

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   config.gem 'andre-geokit', :lib => 'geokit', :source => 'http://gems.github.com'  
   config.gem 'geoip', :version => '0.8.0'
+  config.gem 'flexmock', :version => '0.8.5'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
