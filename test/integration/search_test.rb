15013e42d1844b1995b8421fa59143757b7a9343
Mike Tierney <mike@panpainter.com>
2009-03-10 21:25:24 -0700

    Working towards writing tests for ThinkingSphinx

--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class SearchTest < Test::Unit::TestCase
-  # fixtures :all
-  # 
-  # S = Thinkingsphinx::Search
-  # 
+  fixtures :all
+
+  test "Groups should be indexed" do
+    system("cd #{RAILS_ROOT}; rake ts:stop &> /dev/null")
+
+    assert_nothing_raised do
+      puts "Hi"
+    end
+    
+    system("cd #{RAILS_ROOT}; rake ts:start &> /dev/null")
+    end
+  end
 end
