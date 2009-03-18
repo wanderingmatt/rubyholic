8d832901c2cad0492a8a613d46dab8f5be493ae7
Mike Tierney <mike@panpainter.com>
2009-03-18 14:28:47 -0700

    Cleaning up the search controller a bit.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -19,25 +19,25 @@ class SearchController < ApplicationController
       :order => "date ASC"
     )
     
-    @lat_rads = degrees_to_radians(@ip_location[:latitude].to_f)
-    @long_rads = degrees_to_radians(@ip_location[:longitude].to_f)
-    
-    @groups = Group.search(@query || "")
-    @locations = Location.search(
-      (@query || ""), 
-      :geo => [
-        degrees_to_radians(@ip_location[:latitude].to_f),
-        degrees_to_radians(@ip_location[:longitude].to_f)
-      ],
-      :with => { "@geodist" => 0.0..80467.2 }, # 50 miles, in meters
-      :without => { :start_time => 35.years.ago..Time.now }, # A little hard-coded, but it works well enough for now.
-      :order => "@geodist ASC"
-    )
+    @locations = search_locations(@query)
+    # @groups = Group.search(@query || "")
     
     respond_to do |format|
       format.html # index.html.erb
       format.xml { render :xml => @search }
     end
+  end
+  
+  def search_locations(query)
+    Location.search(
+      (query || ""), 
+      :geo => [
+        degrees_to_radians(@ip_location[:latitude].to_f),
+        degrees_to_radians(@ip_location[:longitude].to_f)
+      ],
+      :with => { "@geodist" => 0.0..80467.2 }, # 50 miles, in meters
+      :order => "@geodist ASC"
+    )
   end
   
   private
