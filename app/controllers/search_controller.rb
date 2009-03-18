bf62c320d36b13286d0234711f6c3cb4364994a2
Mike Tierney <mike@panpainter.com>
2009-03-18 13:16:01 -0700

    Removing crufty code, still working on the geolocation searching.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,9 @@
 class SearchController < ApplicationController
   layout 'default'
-  
+
   # GET /searches
   # GET /searches.xml  
   def index
-    create_map @ip_location, 10
     @query = params[:q]
 
     @search = Event.search(
@@ -15,7 +14,9 @@ class SearchController < ApplicationController
         degrees_to_radians(@ip_location[:latitude].to_f),
         degrees_to_radians(@ip_location[:longitude].to_f)
       ],
-      # :with => { "@geodist" => 0.0..10.0 }, # not really sure how big a radius that is ...
+      # :latitude_attr => @ip_location[:latitude],
+      # :longitude_attr => @ip_location[:longitude],
+      # :with => { "@geodist" => 0.0..80467.2 }, # 50 miles, in meters
       :without => { :start_time => 35.years.ago..Time.now }, # A little hard-coded, but it works well enough for now.
       :order => "date ASC, @geodist ASC"
     )
@@ -38,26 +39,7 @@ class SearchController < ApplicationController
   end
   
   private
-  def degrees_to_radians(degree)
-    degree * Math::PI / 180
-  end
-  
-  def create_map(location, zoom = 15)
-    @map = GMap.new('map')
-    @map.control_init(:large_map_3d => true,:map_type => true, :scale => true)
-    @map.center_zoom_init([location[:latitude],location[:longitude]],zoom)
-    @map.add_map_type_init(GMapType::G_PHYSICAL_MAP)
-    @map.set_map_type_init(GMapType::G_PHYSICAL_MAP)
-    # get_upcoming_markers.each do |marker|
-    #   @map.record_init @map.add_overlay(marker)
-    # end
-  end
-
-  def get_upcoming_markers
-    markers = []
-    Event.upcoming.each do |event|
-      markers << GMarker.new([event.location.latitude, event.location.longitude], :info_window => "<div>#{event.group.name}</div><div>#{event.location.name}</div><div>#{Event.pretty_time(event.start_time)}</div>")
-    end
-    markers
+  def degrees_to_radians(degree) 
+    degree * Math::PI / 180 
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,2 @@
 class Search < ActiveRecord::Base
-  def self.search(search, page)
-      paginate :per_page => 5, :page => page, :conditions => ['name like ?', "%#{search}%"], :order => 'name'
-  end
 end
